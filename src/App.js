bbb1a28f9f296735a3dfb35aca2ef054319aec46
Jakub Wawrzyniak <jakubwawrzyniak6@gmail.com>
2021-11-21 00:13:03 +0100

    First steps: background, fonts, navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import "./App.css";
 
-const reducer = () => {
-  return {};
+const initalState = {
+  appState: "Zaczynamy",
 };
-const store = createStore(reducer);
-function App() {
+
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case "appState/change":
+      return { ...state, appState: action.payload };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer, composeWithDevTools());
+
+function PageSelector() {
+  const pages = [
+    "Zaczynamy",
+    "O tobie",
+    "Ocena powietrza",
+    "Energetyka i Ogrzewanie",
+    "Transport",
+    "Podsumowanie",
+  ];
+  const selector = (state) => pages.findIndex((e) => e === state.appState);
+  const doneId = useSelector(selector);
+  const dispatch = useDispatch();
+  const jsx = pages.map((p, id) => {
+    const className = id > doneId ? "toBeDone" : "";
+    const event = { type: "appState/change", payload: p };
+    return (
+      <button onClick={() => dispatch(event)}>
+        <h4 key={p} className={className}>
+          {p}
+        </h4>
+      </button>
+    );
+  });
+  return <div className="PageSelector">{jsx}</div>;
+}
+
+function Questionare() {
   return (
-    <div>
-      <p>im working :D</p>
-    </div>
+    <main id="quest">
+      <PageSelector />
+    </main>
   );
+}
+
+function App() {
+  return <Questionare />;
 }
 
 export { App, store };
