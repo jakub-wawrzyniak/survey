60a221dcedc4ae02df3b669094d076bfb8e38c50
Jakub Wawrzyniak <jakubwawrzyniak6@gmail.com>
2021-11-21 18:36:56 +0100

    Added a single-choice quesiton component, reduced redundancy

--- src/App.js
+++ src/App.js
@@ -55,9 +55,9 @@ function PageSelector() {
   return <div className="PageSelector">{jsx}</div>;
 }
 
-function Button({ children, isOn, toggle }) {
+function Button({ children, isOn, onClick }) {
   return (
-    <button onClick={toggle} className={isOn ? "on button" : "off button"}>
+    <button onClick={onClick} className={isOn ? "on button" : "off button"}>
       {children}
     </button>
   );
@@ -78,28 +78,66 @@ function action(quesiton, newValue) {
 }
 
 function MultiChoice({ question, howManyAnswers = question.answers.length }) {
+  const getMultiChoiceButtonHandler = (
+    question,
+    answerIds,
+    id,
+    dispatch,
+    howManyAnswers
+  ) => {
+    if (howManyAnswers >= answerIds.length) return () => {};
+    const newAnswerIds = [...answerIds];
+    const isOn = answerIds.includes(id);
+    if (isOn) newAnswerIds.filter((i) => i !== id);
+    else newAnswerIds.push(id);
+    return () => dispatch(action(question, newAnswerIds));
+  };
+
+  return (
+    <QuestionTemplate
+      question={question}
+      howManyAnswers={howManyAnswers}
+      getButtonHandler={getMultiChoiceButtonHandler}
+    />
+  );
+}
+
+function QuestionTemplate({ question, howManyAnswers, getButtonHandler }) {
   const answerIds = useSelector(getAnswerSelector(question), shallowEqual);
   const dispatch = useDispatch();
   const ansBtns = question.answers.map((ans, id) => {
-    const isOn = answerIds.includes(id);
-    const toggle = () => {
-      let newAnswerIds = [...answerIds];
-      if (isOn) newAnswerIds = newAnswerIds.filter((i) => i !== id);
-      else if (answerIds.length >= howManyAnswers) return;
-      else newAnswerIds.push(id);
-      dispatch(action(question, newAnswerIds));
-    };
+    const handleClick = getButtonHandler(
+      question,
+      answerIds,
+      id,
+      dispatch,
+      howManyAnswers
+    );
     return (
-      <Button key={id} isOn={isOn} toggle={toggle}>
+      <Button key={id} isOn={answerIds.includes(id)} onClick={handleClick}>
         <h5 className="longAnswer">{ans}</h5>
       </Button>
     );
   });
   return (
-    <div className={"Multichoice question"}>
+    <div className={"question"}>
       <h4 className="questionHeader">{question.question}</h4>
       <div className="longAnswers">{ansBtns}</div>
     </div>
+  );
+}
+
+function LongSingleChoice({ question }) {
+  const getSingleChoiceButtonHandler = (question, answerIds, id, dispatch) => {
+    const isOn = answerIds === id;
+    const newValue = answerIds[0] === id ? [] : [id];
+    return () => dispatch(action(question, newValue));
+  };
+  return (
+    <QuestionTemplate
+      question={question}
+      getButtonHandler={getSingleChoiceButtonHandler}
+    />
   );
 }
 
@@ -108,6 +146,8 @@ function Question({ question }) {
     switch (question.type) {
       case "multichoice":
         return <MultiChoice question={question} howManyAnswers={2} />;
+      case "long-singlechoice":
+        return <LongSingleChoice question={question} />;
       default:
         break;
     }
@@ -121,6 +161,7 @@ function Questionare() {
       <PageSelector />
       <div id="questContainer">
         <Question question={questions[0]} />
+        <Question question={questions[1]} />
       </div>
     </main>
   );
--- src/questions.js
+++ src/questions.js
@@ -13,6 +13,20 @@ const questions = [
     ],
     default: true,
   },
+  {
+    id: 2,
+    type: "long-singlechoice",
+    question:
+      "Jak oceniasz obecną jakość powietrza w porównaniu do tej sprzed 5 lat?",
+    answers: [
+      "Jest znacznie lepiej",
+      "Jest nieco lepiej",
+      "Bez zmian",
+      "Jest nieco gorzej",
+      "Jest znacznie gorzej",
+    ],
+    default: true,
+  },
 ];
 
 export default questions;
