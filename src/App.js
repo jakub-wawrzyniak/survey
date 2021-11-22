65bf95592e5f6dc01eb9858ea84047eabe8b9feb
Jakub Wawrzyniak <jakubwawrzyniak6@gmail.com>
2021-11-22 15:53:55 +0100

    Connected frontend with firestore: now answers can be sent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { createStore } from "redux";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { composeWithDevTools } from "redux-devtools-extension";
 import "./App.css";
 import { questions, pages } from "./texts";
 import Question from "./Question/Question";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcqGLhgPjiDmes0WCk25fH32cgKSZHNBQ",
+  authDomain: "test-7c874.firebaseapp.com",
+  projectId: "test-7c874",
+  storageBucket: "test-7c874.appspot.com",
+  messagingSenderId: "467835212184",
+  appId: "1:467835212184:web:22b2c6695b64acbc159bed",
+};
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
 function getEmptyAnswers() {
   const out = {};
@@ -14,6 +33,7 @@ function getEmptyAnswers() {
 
 const initalState = {
   appState: "Zaczynamy",
+  answersSent: false,
   answers: getEmptyAnswers(),
 };
 
@@ -25,6 +45,8 @@ const reducer = (state = initalState, action) => {
       const newState = { ...state, answers: { ...state.answers } };
       newState.answers[action.payload.id] = action.payload.value;
       return newState;
+    case "answers/sent":
+      return { ...state, answersSent: true };
     default:
       return state;
   }
@@ -129,6 +151,42 @@ function StartSurvey() {
   );
 }
 
+function SendButton() {
+  const answersSent = useSelector((s) => s.answersSent);
+  const [loading, setLoading] = useState(false);
+  const send = async () => {
+    try {
+      const answers = store.getState().answers;
+      const multiPointQuests = questions.filter((q) => q.type === "multipoint");
+      multiPointQuests.forEach((q) =>
+        q.answers.forEach((_, id) => {
+          if (answers[q.id][id] === undefined) answers[q.id][id] = null;
+        })
+      );
+      setLoading(true);
+      await addDoc(collection(db, "answers"), answers);
+      store.dispatch({ type: "answers/sent" });
+    } catch (e) {
+      console.error("Error while sending answers: ", e);
+    }
+  };
+
+  let [accent, text, onClick] = [true, "Wyślij odpowiedzi", send];
+  if (answersSent)
+    [accent, text, onClick] = [
+      false,
+      "Odpowiedzi wysłane - dziękujemy :)",
+      () => {},
+    ];
+  else if (loading)
+    [accent, text, onClick] = [false, "Wysyłanie odpowiedzi...", () => {}];
+  return (
+    <ControlButton onClick={onClick} accent={accent}>
+      <h4>{text}</h4>
+    </ControlButton>
+  );
+}
+
 function EndSurvey() {
   const dispatch = useDispatch();
   const handleClick = () =>
@@ -160,6 +218,7 @@ function EndSurvey() {
           </li>
         </ul>
       </div>
+      <SendButton />
       <ControlButton onClick={handleClick}>
         <h4>Powrót do pytań</h4>
       </ControlButton>
@@ -186,9 +245,6 @@ function Questionare() {
             <Question question={q} key={q.id} />
           ))}
         <Element />
-        {/* {pageId === 0 && <StartSurvey />}
-        {pageId === pages.length - 1 && <EndSurvey />}
-        {pageId !== 0 && pageId !== pages.length - 1 && <ControlButtons />} */}
       </div>
     </main>
   );
